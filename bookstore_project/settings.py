f9df05595bee71d3c1e82a3f07a0cbe6733d26ae
suman <sumanaswal818@gmail.com>
2020-06-30 01:20:08 +0530

    added media file

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -140,6 +140,9 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # login 
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
